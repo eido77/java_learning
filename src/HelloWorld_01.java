f03b17cd2fa151287c02562c2d35a9555e003a89
Eido <eido77@proton.me>
2026-05-22 19:54:13 +0800

    Initial commit

---
+++ src/HelloWorld_01.java
@@ -0,0 +1,21 @@
+//class关键字，表示“类“
+//Option + Command + L一次性整理整个文件的缩进（可以选中，不选中就是全部）
+//public的class后面名字和java的名字一样，最多用一个public
+public class HelloWorld_01 {
+    //psvm
+    public static void main(String[] args) {
+        //字符串必须用一对双引号 " " 完整闭合，" 一旦开始就必须在同一行内闭合
+        //sout，每行执行语句必须用;结尾
+        System.out.println("Hello World");
+        //去掉ln，输出语句之后不会换行
+        System.out.print(123);
+        System.out.println("Hello");
+        System.out.println("abc");
+    }
+}
+
+class Hello1 {
+}
+
+class Hello2 {
+}
