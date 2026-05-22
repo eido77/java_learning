f4928fb66bc49fa9910fe16e18ba403e483054ec
Eido <eido77@proton.me>
2026-05-22 23:15:49 +0800

    Initial commit

---
+++ src/Star_03.java
@@ -0,0 +1,10 @@
+//println 自己就会在最后自动换行。
+public class Star_03 {
+    public static void main(String[] args) {
+        System.out.println("*    *");
+        //"\t"表示一定长度的空格
+        System.out.println("*\t*");
+        //"\n"表示换行
+        System.out.println("\n\n*");
+    }
+}
